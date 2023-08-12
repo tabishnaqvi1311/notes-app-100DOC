eefaf512d73cab703bcbff812a6e3c5863016473
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-08-12 21:45:36 +0530

    using cors

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express()
 const db = require("./db");
 const port = 8181;
+const cors = require("cors");
 
 const authRoute = require("./routes/auth.js");
 const noteRoute = require("./routes/notes.js");
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoute);
 app.use("/api/notes", noteRoute);
